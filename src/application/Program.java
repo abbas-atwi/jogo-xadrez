9d4eb86b717bab7a68448e5903c9b47c9dd2cb65
Abbas Atwi <abbas_9589_new@hotmail.com>
2020-09-27 20:17:24 -0300

    Classe posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,15 @@ package application;
 
 import java.util.Locale;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-		System.out.println("Hello world!");
-		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 
 	}
 
